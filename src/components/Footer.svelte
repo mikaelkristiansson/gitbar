a9bd07505bf8c4bfa0cc9547a6cd3e16f9ebf5ef
Mikael Kristiansson <mikael.kristiansson87@gmail.com>
2024-09-13 22:05:55 +0200

    fix: update icon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -88,7 +88,8 @@
           width="24"
           height="24"
           class="w-6 h-6"
-          stroke="currentColor"
+          stroke="none"
+          fill="currentColor"
           ><path
             d="M2.75 6a.75.75 0 0 0 0 1.5h18.5a.75.75 0 0 0 0-1.5H2.75ZM6 11.75a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H6.75a.75.75 0 0 1-.75-.75Zm4 4.938a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"
           ></path></svg
