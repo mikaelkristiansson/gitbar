d316517bb9606e400d4b23dc664b06fbfed3cf54
Mikael Kristiansson <mikael.kristiansson87@gmail.com>
2024-10-04 11:08:04 +0200

    fix: if user is deleted, show ghost

--- src/lib/window-size.ts
+++ src/lib/window-size.ts
@@ -2,7 +2,7 @@ import { appWindow, LogicalSize } from '@tauri-apps/api/window';
 const DEFAULT_HEIGHT = 400;
 const DEFAULT_WIDTH = 500;
 const FOOTER_HEIGHT = 56;
-const MIN_HEIGHT = 100;
+const MIN_HEIGHT = 120;
 
 export const resetWindowSize = async () => await appWindow.setSize(new LogicalSize(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 
